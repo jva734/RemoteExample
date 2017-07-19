78fd4da11e31e8ddc86cbcd3bfa023cd584f8399
John <jalton@stanford.edu>
2017-07-19 13:49:49 -0700

    New class added

---
+++ RemoteExample/src/Push.java
@@ -0,0 +1,13 @@
+
+public class Push {
+
+	public Push() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "Example";
+	}
+
+}
